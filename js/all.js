14ad554d38204be7d73a2ea53f52c326a410c256
aenelle <aenelle@gmail.com>
2018-09-27 19:02:35 +0300

    fix

--- js/all.js
+++ js/all.js
@@ -113,14 +113,6 @@
             case 'email':
                 pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
                 break;
-
-            case 'login':
-                pattern = /^(?=.*[A-Za-z]$)[A-Za-z][A-Za-z\d.-]{0,19}$/;
-                break;
-
-            case 'phone':
-                pattern = /^[0-9]{11,11}$/;
-                break;
         }
 
         return pattern;
@@ -132,6 +124,15 @@
         var formInst = new Form(form);
         formInst.onSubmit(function(e) {
             window.open('ty_page.html', '_black');
+        });
+    });
+
+
+    $('.go-go').on('click', function() {
+        var body = $("html, body");
+        
+        body.stop().animate({scrollTop: $('.pyramid-bg').offset().top}, 500, 'swing', function() { 
+ 
         });
     });
 
